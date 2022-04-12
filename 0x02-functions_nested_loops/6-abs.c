5bb1c1415b43364d2ad44231f7c0625171fc6625
Mulugeta Achiso <anem1351@gmail.com>
2022-04-12 01:39:23 -0700

    print absolute valua of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * -abs - check main.
+  * @c: An input character
+  * Description: Function return absolute value of a number
+  * Return:  return absolute value of a number
+  */
+int _abs(int)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
