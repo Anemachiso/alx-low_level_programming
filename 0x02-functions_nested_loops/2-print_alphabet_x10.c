aa989754a4729ba11476274d554348dfcdc9e519
Mulugeta Achiso <anem1351@gmail.com>
2022-04-12 00:40:50 -0700

    Print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
