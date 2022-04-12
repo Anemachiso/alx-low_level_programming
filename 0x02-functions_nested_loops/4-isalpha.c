cb04156048997050aec96b1eaf451c42299ee599
Mulugeta Achiso <anem1351@gmail.com>
2022-04-12 03:04:04 -0700

    print sign

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-  * isalpha - check main.
-  * @c: An input character
-  * Description: Function returns 1 if the character is a 
+  * _isalpha - check main.
+  * @c: An input character.
+  * Description: Function returns 1 if the character is a
   * letter, lowercase or uppercase.
   * Return: 1 or 0 in otherwise.
   */
@@ -12,7 +12,7 @@ int _isalpha(int c)
 	char lower, upper;
 	int isletter = 0;
 
-	for (lower = 'a'; lower <='z'; lower++)
+	for (lower = 'a'; lower <= 'z'; lower++)
 	{
 		for (upper = 'A'; upper <= 'Z'; upper++)
 		{
