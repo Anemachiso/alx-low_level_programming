11ee7f477a34785938a5f2a6d0f449a872631bf5
Mulugeta Achiso <anem1351@gmail.com>
2022-04-13 05:55:41 -0700

    N Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,40 +8,40 @@
 */
 void print_times_table(int n)
 {
-        int i, j, p;
+	int i, j, p;
 
 	if (n >= 0 && n <= 15)
 	{
-          for (i = 0; i <= n; i++)
-          {
-                  for (j = 0; j <= n; j++)
-                  {
-                          p = i * j;
- 
-                          if ((p / 10) == 0)
-                          {
-                                  if (j != 0)
-                                          _putchar(' ');
-                                  _putchar(p + '0');
- 
-                                  if (j == n)
-                                          continue;
-                                  _putchar(',');
-                                  _putchar(' ');
-                          }
- 
-                          else
-                          {
-                                  _putchar((p / 10) + '0');
-                                  _putchar((p % 10) + '0');
-                                  if (j == n)
-                                          continue;
-                                  _putchar(',');
-                                  _putchar(' ');
- 
-                          }
-                  }
-		  _putchar('\n');
-         }
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				p = i * j;
+
+				if ((p / 10) == 0)
+				{
+					if (j != 0)
+						_putchar(' ');
+					_putchar(p + '0');
+					if (j == n)
+						continue;
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+
+				else
+				{
+					_putchar((p / 10) + '0');
+					_putchar((p % 10) + '0');
+					if (j == n)
+						continue;
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
 	}
 }
