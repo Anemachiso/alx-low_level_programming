aff867c7e99aacce150fed9071b82db09a53876f
Mulugeta Achiso <anem1351@gmail.com>
2022-04-14 09:22:50 -0700

    Print the given number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_number - prints a number
  * @n: Input number
@@ -11,14 +12,13 @@ void print_number(int n)
 
 	res = n;
 	expo = len =  1;
-/*Check negatives*/
+
 	if (res < 0)
 	{
 		res *= -1;
 		_putchar('-');
 	}
 
-/**/
 	temp = res;
 	while (temp >= 10)
 	{
@@ -26,14 +26,12 @@ void print_number(int n)
 		temp /= 10;
 	}
 
-/*Create Exponent*/
 	for (i = 1; i < len; i++)
 		expo *= 10;
-/*Main */
 	while (expo > 1)
 	{
 		_putchar((res / expo) % 10 + '0');
 		expo /= 10;
 	}
-	_putchar(res % 10 + '0');
+	_putchar((res % 10) + '0');
 }
