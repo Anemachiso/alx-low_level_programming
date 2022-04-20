a4dce0469a2f692d31c446dfb68165de87217684
Mulugeta Achiso <anem1351@gmail.com>
2022-04-20 09:02:48 -0700

    Printing number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,14 +14,16 @@ int _putchar(char c);
 
 void my_putchar(long n) 
 {
+	unsigned int n1;
+
+	n1 = n;
+
 	if (n < 0) 
 	{
 		putchar('-');
-		n = n * -1;
+		n1 = -n;
 	}
-	if (n == 0)
-		putchar('0');
-	if (n / 10)
-		my_putchar(n / 10);
-	putchar(n % 10 + '0');
+	if (n1 / 10 != 0)
+		 my_putchar(n1 / 10);
+	putchar((n1 % 10) + '0');
 }
