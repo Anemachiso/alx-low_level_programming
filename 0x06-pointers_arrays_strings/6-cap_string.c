219383e5a0303c666fa0c13c7a441057ebb75d63
Mulugeta Achiso <anem1351@gmail.com>
2022-04-20 08:50:30 -0700

    Capitalizing the first letter of a word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * cap_string - a function that capitalizes all words of a string.
- * @s: An input string to capitalize letters
- * Return: pointer to s
+ * *cap_string - this is awesome
+ * @s: pointer to char params
+ *
+ * Return: *s
  */
+
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	char delimeters[] = " \t\n,;.!?\"(){}";
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0)
-		{
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] -= 32;
-			continue;
-		}
-		if (s[i] == ' ')
-		{
-			++i;
-			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				s[i] -= 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (s[i] >= 'A' && s[i] <= 'Z')
-				s[i] += 32;
-		}
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+				for (j = 0; delimeters[j] != '\0'; j++)
+					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
 	}
-	
 	return (s);
 }
