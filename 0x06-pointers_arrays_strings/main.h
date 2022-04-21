c3da567ba36c5e5e378ad6da61bc6ff845c2f408
Mulugeta Achiso <anem1351@gmail.com>
2022-04-21 08:57:11 -0700

    main header

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
@@ -9,3 +12,5 @@ char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
+
+#endif
