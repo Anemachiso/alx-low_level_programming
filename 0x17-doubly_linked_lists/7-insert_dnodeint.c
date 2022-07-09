b28a4393d12d62bc46bc819305ba5da11b297bd2
Mulugeta Achiso <anem1351@gmail.com>
2022-07-09 06:11:19 -0700

    add node randomly

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (idx == 1)
-		dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+		dlistint_t *add_dnodeint(head, n);
 
 	new = malloc(sizeof(dlistint_t));
 
@@ -45,4 +45,66 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current->prev->next = new;
 
 	return (current);
+}
+
+/**
+ * *add_dnodeint - adds a node to a linked list head
+ * @head: linked list head
+ * @n: int element of the linked list
+ * Return: number of elements
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+    new_node->prev = NULL;
+	*head = new_node;
+
+	return (new_node);
+}
+
+/**
+ * add_dnodeint_end - add a node at the end of a linked list
+ * @head: The character to print
+ * @n: string for the new node
+ *
+ * Return: new node
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *current;
+	dlistint_t *new_node;
+
+	current = *head;
+	while (current && current->next != NULL)
+		current = current->next;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+    new_node->prev = current;
+
+	if (current)
+		current->next = new_node;
+	else
+		*head = new_node;
+
+	return (new_node);
 }
